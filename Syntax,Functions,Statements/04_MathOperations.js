f742fb9eb78a2d0ddd9b4b98b60b7e8055c4484d
SimeonKazandzhiev <simeon.tu@gmail.com>
2021-04-13 10:18:19 +0200

    added solved problems

---
+++ Syntax,Functions,Statements/04_MathOperations.js
@@ -0,0 +1,27 @@
+function mathOperations(number1, number2, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+             result = number1 + number2;
+             break;
+        case '-':
+            result = number1 - number2;
+             break;
+        case '/':
+            result = number1 / number2;
+             break;
+        case '*':
+            result = number1 * number2;
+             break;
+        case '%':
+            result = number1 % number2;
+             break;
+        case '**':
+            result = number1 ** number2;
+             break;
+    }
+    console.log(result);
+}
+
+mathOperations(2,2,'-');
